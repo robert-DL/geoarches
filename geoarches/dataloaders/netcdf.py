fix nan masking in loss

geoarches/dataloaders/netcdf.py
@@ -198,8 +198,8 @@ class XarrayDataset(torch.utils.data.Dataset):
         return tdict
 
     def __getitem__(self, i, return_timestamp=False, interpolate_nans=None, warning_on_nan=None):
-        interpolate_nans = interpolate_nans or self.interpolate_nans
-        warning_on_nan = warning_on_nan or self.warning_on_nan
+        interpolate_nans = self.interpolate_nans if interpolate_nans is None else interpolate_nans
+        warning_on_nan = self.warning_on_nan if warning_on_nan is None else warning_on_nan
 
         file_id, line_id, timestamp = self.id2pt[i]
 

geoarches/lightning_modules/forecast.py
@@ -135,8 +135,11 @@ class ForecastModule(BaseLightningModule):
         weighted_error = (pred - gt).abs().pow(self.pow).mul(loss_coeffs)
 
         # Mask loss where gt is NaN.
-        target_valid = tensordict_apply(lambda x: torch.where(torch.isnan(x), 0.0, 1.0), gt)
-        masked_loss = (weighted_error * target_valid) / target_valid.mean()
+        target_valid = tensordict_apply(lambda x: ~torch.isnan(x), gt)
+        weighted_error = tensordict_apply(
+            lambda mask, val: torch.where(mask, val, 0.0), target_valid, weighted_error
+        )
+        masked_loss = (weighted_error) / target_valid.float().mean()
 
         loss = sum(masked_loss.mean().values())
 